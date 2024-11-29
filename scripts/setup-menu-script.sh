b866bafe2df53c6475370aff76a330f7cffa8d00
Hayder <macbook@MacBookPro.MG8702>
2024-11-29 11:03:10 -0800

    add bugy server setup role

--- scripts/setup-menu-script.sh
+++ scripts/setup-menu-script.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
 echo "Select a playbook to run:"
-echo "1) Web Server Setup"
-echo "2) Database Server Setup"
-echo "3) Ansible Server Setup"
-echo "4) Exit"
+echo "1) Ansible Server Setup"
+echo "2) Webservers Setup"
+echo "3) Chat Server Setup"
+echo "4) Nagios Server Setup"
+echo "5) Log Server Setup"
+echo "6) Exit"
 
 read -p "Enter your choice [1-4]: " choice
 
 case $choice in
     1)
-        ansible-playbook /usr/share/bugy/scripts/webserver-setup.yml
+        ansible-playbook /opt/ansible/playbooks/ansible_node/main_playbook.yaml
         ;;
     2)
-        ansible-playbook /usr/share/bugy/scripts/database-server-setup.yml
+        ansible-playbook /opt/ansible/playbooks/webservers/main_playbook.yaml
         ;;
     3)
-        ansible-playbook /usr/share/bugy/scripts/ansible-server-setup.yml
+        ansible-playbook /opt/ansible/playbooks/chat_server/main_playbook.yaml
         ;;
     4)
+        ansible-playbook /opt/ansible/playbooks/ # Need fix 
+        ;;
+    5)
+        ansible-playbook /opt/ansible/playbooks/ # Not ready yet 
+        ;;
+    6)
         echo "Exiting..."
         exit 0
         ;;
