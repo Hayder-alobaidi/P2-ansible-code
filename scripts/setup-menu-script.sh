6d159dc6bfdb6936017efb758a4286eca9ed49ea
Hayder <macbook@MacBookPro.MG8702>
2024-12-02 05:55:15 -0800

    add bugy server setup role

--- scripts/setup-menu-script.sh
+++ scripts/setup-menu-script.sh
@@ -2,56 +2,87 @@
 
 # Ensure Ansible's color output is enabled
 export ANSIBLE_FORCE_COLOR=1
+export ANSIBLE_NOCOLOR=0  # Ensure color is enabled
 
+# Color variables
+BLUE="\033[1;34m"
+GREEN="\033[1;32m"
+RED="\033[1;31m"
+CYAN="\033[1;36m"
+PURPLE="\033[1;35m"
+RESET="\033[0m"
+
+# Function to display the menu
+display_menu() {
+    echo -e "\n${BLUE}Select a playbook to run:${RESET}"
+    echo -e "${GREEN}1)${RESET} Ansible Server Setup"
+    echo -e "${GREEN}2)${RESET} Webservers Setup"
+    echo -e "${GREEN}3)${RESET} Chat Server Setup"
+    echo -e "${GREEN}4)${RESET} Nagios Server Setup"
+    echo -e "${GREEN}5)${RESET} Log Server Setup"
+    echo -e "${RED}6)${RESET} Exit"
+    echo -n -e "\n${CYAN}Enter your choice [1-6]: ${RESET}"
+}
+
+# Function to run playbook
+run_playbook() {
+    playbook_path=$1
+    echo -e "\n${PURPLE}Running playbook: $playbook_path${RESET}"  # Purple color
+    ansible-playbook "$playbook_path"
+}
+
+# Main loop
 while true; do
-    # Show the menu
-    echo "Select a playbook to run:"
-    echo "1) Ansible Server Setup"
-    echo "2) Webservers Setup"
-    echo "3) Chat Server Setup"
-    echo "4) Nagios Server Setup"
-    echo "5) Log Server Setup"
-    echo "6) Exit"
-    
-    read -p "Enter your choice [1-6]: " choice
+    display_menu  # Show the menu
+
+    if ! read -t 60 -p "" choice; then
+        echo -e "\n${RED}Timeout! No input received. Exiting...${RESET}"  # Red color
+        exit 1
+    fi
 
     case $choice in
         1)
-            ansible-playbook  /opt/ansible/playbooks/ansible_node/main_playbook.yaml
+            echo -e "\n${RED}You selected 'Ansible Server Setup'. This will configure your Ansible nodes.${RESET}"  # Red color
+            run_playbook "/opt/ansible/playbooks/ansible_node/main_playbook.yaml"
             ;;
         2)
-            ansible-playbook /opt/ansible/playbooks/webservers/main_playbook.yaml
+            echo -e "\n${RED}You selected 'Webservers Setup'. This will configure web servers.${RESET}"  # Red color
+            run_playbook "/opt/ansible/playbooks/webservers/main_playbook.yaml"
             ;;
         3)
-            ansible-playbook /opt/ansible/playbooks/chat_server/main_playbook.yaml
+            echo -e "\n${RED}You selected 'Chat Server Setup'. This will set up the chat server.${RESET}"  # Red color
+            run_playbook "/opt/ansible/playbooks/chat_server/main_playbook.yaml"
             ;;
         4)
-            ansible-playbook /opt/ansible/playbooks/nagios_server/main_playbook.yaml #Need fix 
+            echo -e "\n${RED}You selected 'Nagios Server Setup'. This will set up Nagios monitoring.${RESET}"  # Red color
+            run_playbook "/opt/ansible/playbooks/nagios_server/main_playbook.yaml"
             ;;
         5)
-            ansible-playbook /opt/ansible/playbooks/log_server/main_playbook.yaml  # Not ready yet
+            echo -e "\n${RED}You selected 'Log Server Setup'. This will configure the log server.${RESET}"  # Red color
+            run_playbook "/opt/ansible/playbooks/log_server/main_playbook.yaml"
             ;;
         6)
-            echo "Exiting..."
+            echo -e "\n${RED}Exiting... Goodbye! Have a nice day!${RESET}"  # Red color
             exit 0
             ;;
         *)
-            echo "Invalid choice. Please select a number between 1 and 6."
+            echo -e "\n${RED}Invalid choice. Please enter a number between 1 and 6.${RESET}"  # Red color
             ;;
     esac
 
     # Ask the user if they want to return to the menu or exit
     read -p "Would you like to go back to the menu? (y/n): " go_back
     if [[ "$go_back" != "y" && "$go_back" != "Y" ]]; then
-        echo "Exiting..."
+        echo -e "\n${RED}Exiting... Goodbye! See you next time.${RESET}"  # Red color
         exit 0
     fi
 done
 
 
 
-# ###
 
+
+# NOTEs:
 # put this for the server URL for the script to work 
 
 # Script name:
@@ -59,4 +90,3 @@ done
 
 # Script path:
 # /opt/ansible/scripts/setup-menu-script.sh
-
